ae8907bf75374e7128dd5aa5e6e49b44e7e1d1a9
Hady-Hassan <90941057+Hady-Hassan@users.noreply.github.com>
2022-04-01 23:49:46 +0200

    linking activities

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,7 +20,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener );
 
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener  =
+            new BottomNavigationView.OnNavigationItemSelectedListener()
+            {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item)
+                {
+                    Fragment selectedFargment = null;
+                    switch (item.getItemId())
+                    {
+                        case R.id.home:
+                            selectedFargment = new Home();
+                            break;
+                        case R.id.cartfrag:
+                            selectedFargment = new Cart();
+                            break;
+                        case R.id.profile:
+                            selectedFargment = new Profile();
+                            break;
+                        case R.id.offers:
+                            selectedFargment = new Offers();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,selectedFargment).commit();
+                    return true;
+                }
+            };
 }
