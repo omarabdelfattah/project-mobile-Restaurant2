a3f7273e1b0ce92cd9aaa6837fa82a321569342c
Mk662000 <mk840166@gmail.com>
2022-04-20 03:14:53 +0200

    sign_up page

--- app/src/main/java/com/example/project/SignUpActivity.java
+++ app/src/main/java/com/example/project/SignUpActivity.java
@@ -2,11 +2,30 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    EditText UserName, PassWord, ConPassWord, EmailAddress, Phone;
+
+
+public void Inputuser(View v) {
+
+    String Username = UserName.getText().toString();
+    String PassWord = UserName.getText().toString();
+    String EmailAddress = UserName.getText().toString();
+    String Phone = UserName.getText().toString();
+
+    Account InputUser = new Account(Username, PassWord, Phone, EmailAddress);
+    Account.users.add(InputUser);
+    Intent intent = new Intent(this, log_in.class);
+
+    startActivity(intent);
+    }
 
     private Button signUp;
     @Override
@@ -18,6 +37,16 @@ public class SignUpActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+
+         UserName = (EditText) findViewById(R.id.susername);
+         ConPassWord = (EditText) findViewById(R.id.spassword);
+         PassWord = (EditText) findViewById(R.id.sphonenum);
+         EmailAddress = (EditText) findViewById(R.id.semail);
+         Phone = (EditText) findViewById(R.id.sphonenum);
+
+
+
 
 
     }
