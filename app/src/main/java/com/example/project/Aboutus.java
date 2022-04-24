7a5f5a9d13c4afa82774ad57d48494c110305d71
OmarAhmedAliAbdulMaj <sim.omarahmed3077@alexu.edu.eg>
2022-04-24 22:49:25 +0200

    About Us v2 Feedback text rating bar

--- app/src/main/java/com/example/project/Aboutus.java
+++ app/src/main/java/com/example/project/Aboutus.java
@@ -1,7 +1,12 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +17,53 @@ public class Aboutus extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_aboutus);
+
+        TextView tvFeedback = findViewById(R.id.tvFeedback);
+        RatingBar rbStars = findViewById(R.id.rbStars);
+        rbStars.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if(rating==0)
+                {
+                    tvFeedback.setText("Very Dissatisfied");
+                }
+                else if(rating==1)
+                {
+                    tvFeedback.setText("Dissatisfied");
+                }
+                else if(rating==2 || rating==3)
+                {
+                    tvFeedback.setText("OK");
+                }
+                else if(rating==4)
+                {
+                    tvFeedback.setText("Satisfied");
+                }
+                else if(rating==5)
+                {
+                    tvFeedback.setText("Very Satisfied");
+                }
+                else
+                {
+
+                }
+            }
+        });
+
+            Button location = findViewById(R.id.btn_Location);
+            location.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri gmmIntentUri = Uri.parse("google.navigation:q=31.207553,29.918972");
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    startActivity(mapIntent);
+
+                }
+            });
+
+
+
     }
 
 }
